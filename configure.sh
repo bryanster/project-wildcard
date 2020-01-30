a030621cfb10263858599badfa8b8981f6215566
bryanster <45668775+bryanster@users.noreply.github.com>
2020-01-30 22:49:38 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,6 +4,9 @@ echo "what is your wan interface"
 read wanint
 echo "what is your lan interface"
 read lanint
+rcctl enable dhcpd
+rcctl set dhpd flags $lanint
+echo "inet 192.168.1.254 255.255.255.0 NONE" > /etc/hostname.$lanint
 echo "wan = $wanint" > /etc/pf.conf
 echo "lan = $lanint" >> /etc/pf.conf
 cat ./config/pf.conf >> /etc/pf.conf
