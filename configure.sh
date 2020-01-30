daf089414b891e411043575a9d8ba5c2c23e184a
bryanster <45668775+bryanster@users.noreply.github.com>
2020-01-30 22:10:43 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,3 +1,9 @@
 #configure router 
 echo 'net.inet.ip.forwarding=1' >> /etc/sysctl.conf
-cp ./config/pf.conf /etc/pf.conf
+echo "what is your wan interface"
+read wanint
+echo "what is your lan interface"
+read lanint
+echo "wan = $wanint" > /etc/pf.conf
+echo "lan = $lanint" >> /etc/pf.conf
+cat./config/pf.conf >> /etc/pf.conf
