585077d83cc6822d5527a7537dfa1bef3460726e
bryanster <45668775+bryanster@users.noreply.github.com>
2020-02-27 20:20:48 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -15,8 +15,8 @@ cat ./config/pf.conf >> /etc/pf.conf
 #configure capture
 mkdir /capture
 chmod 777 /capture
-echo "tcpdump -i $wanint -G 60 -w /capture/wan_%d-%m_%Y__%H_%M.pcap" >> ./bin/capture.sh
-echo "tcpdump -i $lanint -G 60 -w /capture/lan_%d-%m_%Y__%H_%M.pcap" >> ./bin/capture.sh
+echo "tcpdump -i $wanint -G 60 -w /capture/wan_%d-%m_%Y__%H_%M.pcap" >> ./bin/capture
+echo "tcpdump -i $lanint -G 60 -w /capture/lan_%d-%m_%Y__%H_%M.pcap" >> ./bin/capture
 cp ./bin/capture.sh /bin/capture
 chmod +x /bin/capture
 cp /config/dhcpd.conf /etc/dhcpd.conf
