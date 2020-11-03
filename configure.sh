44c9e3c3bea6d01f88e6c259a30365e6c7e28bf5
bryan@nexezo.com <bryan@LTNEX01.localdomain>
2020-11-03 20:49:01 +0100

    added unbound

--- configure.sh
+++ configure.sh
@@ -7,10 +7,16 @@ echo "what is your lan interface"
 read lanint
 rcctl enable dhcpd
 rcctl set dhcpd flags $lanint
-echo "inet 192.168.1.254 255.255.255.0 NONE" > /etc/hostname.$lanint
+echo "inet 192.168.255.254 255.255.255.0 NONE" > /etc/hostname.$lanint
 echo "wan = $wanint" > /etc/pf.conf
 echo "lan = $lanint" >> /etc/pf.conf
 cat ./config/pf.conf >> /etc/pf.conf
+cp ./config/dhcpd.conf /etc/dhcpd.conf
+
+
+#setup unbound
+rcctl enable unbound
+cp ./config/unbound.conf /var/unbound/etc/unbound.conf
 
 #configure capture
 mkdir /capture
@@ -19,4 +25,3 @@ echo "tcpdump -i $wanint -G 60 -w /capture/wan_%d-%m_%Y__%H_%M.pcap" >> ./bin/ca
 echo "tcpdump -i $lanint -G 60 -w /capture/lan_%d-%m_%Y__%H_%M.pcap" >> ./bin/capture
 cp ./bin/capture /bin/capture
 chmod +x /bin/capture
-cp ./config/dhcpd.conf /etc/dhcpd.conf
