f4dc79060aa0cb0a9c5b466cb06552c32007965a
bryan@nexezo.com <bryan@LTNEX01.localdomain>
2020-11-06 21:47:10 +0100

    update

--- configure.sh
+++ configure.sh
@@ -14,9 +14,12 @@ cat ./config/pf.conf >> /etc/pf.conf
 cp ./config/dhcpd.conf /etc/dhcpd.conf
 
 
+
 #setup unbound
 rcctl enable unbound
 cp ./config/unbound.conf /var/unbound/etc/unbound.conf
+cp ./config/blacklist.conf /var/unbound/etc/blacklist.conf
+
 
 #configure capture
 mkdir /capture
@@ -25,3 +28,23 @@ echo "tcpdump -i $wanint -G 60 -w /capture/wan_%d-%m_%Y__%H_%M.pcap" >> ./bin/ca
 echo "tcpdump -i $lanint -G 60 -w /capture/lan_%d-%m_%Y__%H_%M.pcap" >> ./bin/capture
 cp ./bin/capture /bin/capture
 chmod +x /bin/capture
+
+#configure httpd
+cp ./config/httpd.conf /etc/httpd.conf
+rcctl enable httpd
+rcctl start httpd
+mkdir /var/www/htdocs/blocked
+cp ./Blocked/* /var/www/htdocs/blocked/
+
+
+# add blacklist to cron
+chmod +x ./blacklist.sh
+cp ./blacklist.sh /bin/blacklist.sh
+
+#write out current crontab
+crontab -l > mycron
+#echo new cron into cron file
+echo "* * * * * /bin/blacklist.sh >/dev/null 2>&1" >> mycron
+#install new cron file
+crontab mycron
+rm mycron
