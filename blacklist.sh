6c7372f239d4b65ee62e7a923b0cae13a5cf2887
bryanster <bnet1998@gmail.com>
2021-07-30 18:40:26 +0200

    changed blacklist creation

--- blacklist.sh
+++ blacklist.sh
@@ -9,8 +9,10 @@ git pull
 #create ublacklist
 rm /etc/brienlist/ublacklist
 touch /etc/brienlist/ublacklist
-for p in (dblacklist)
-do
-    echo "local-data: ${p} A 192.168.255.254" >> /etc/brienlist/ublacklist
-done
 
+file="/etc/brienlist/dblacklist"
+
+while IFS= read -r line
+do
+    echo "local-data: $line A 192.168.255.254" >> /etc/brienlist/ublacklist
+done 
