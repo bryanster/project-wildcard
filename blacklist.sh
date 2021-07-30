d3d101dbb0e33f389590f2eef69e7763e13e21e3
bryanster <bnet1998@gmail.com>
2021-07-30 18:34:06 +0200

    updated unbound config and blacklist update script

--- blacklist.sh
+++ blacklist.sh
@@ -6,3 +6,10 @@
 #
 cd /etc/brienlist
 git pull
+#create ublacklist
+rm /etc/brienlist/ublacklist
+for p in (dblacklist)
+do
+    echo "local-data: ${p} A 192.168.255.254" >> /etc/brienlist/ublacklist
+done
+
