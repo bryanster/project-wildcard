a82a79a7502a376e2da00ebcb35e36270edf1092
bryanster <bnet1998@gmail.com>
2021-07-30 22:44:30 +0200

    added express web server

--- Webint/index.js
+++ Webint/index.js
@@ -2,19 +2,52 @@
 //  Title:         webinterface
 //  Author:        bryanster
 //  Version:       1.0
+var express = require('express'),
+    routes = require('./routes'),
+    upload = require('./routes/upload'),
+    http = require('http'),
+    https = require('https'),
+    fs = require('fs'),
+    path = require('path'),
+    httpApp = express(),
+    app = express(),
+    certPath = "/etc/webui/";
 
-const express = require('express');
-var os = require("os");
-const app = express()
-async function main() {
-    var port = 4000
-    var hostname = os.hostname();
-    app.get("/", async (req, res) => {
-        res.send(`this is server: ${hostname} `);
-    })
-    app.listen(port, () => {
-        console.log(`Brynet listening at http://localhost:${port}`)
-    })
+
+var httpsOptions = {
+    key: fs.readFileSync(path.join(certPath, "server.key")),
+    cert: fs.readFileSync(path.join(certPath, "server.crt"))
+};
+httpApp.set('port', process.env.PORT || 80);
+httpApp.get("*", function (req, res, next) {
+    res.redirect("https://" + req.headers.host + "/" + req.path);
+});
+
+// all environments
+app.set('port', process.env.PORT || 443);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.enable('trust proxy');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if('development' == app.get('env')) {
+    app.use(express.errorHandler());
 }
 
-main();
+app.get('/', routes.index);
+app.post('/upload', upload.s3);
+
+
+http.createServer(httpApp).listen(httpApp.get('port'), function() {
+    console.log('Express HTTP server listening on port ' + httpApp.get('port'));
+});
+
+https.createServer(httpsOptions, app).listen(app.get('port'), function() {
+    console.log('Express HTTPS server listening on port ' + app.get('port'));
+});
