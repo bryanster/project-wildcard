97b2468d001365c8ea46a8e73a82a63c1192d384
bryanster <bnet1998@gmail.com>
2021-08-21 22:14:04 +0200

    changed to https

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,17 @@
 //  Title:        Express backend 
 //  Author:        Bryan van der Net
 //  Version:       1.0
-const configfile = require('/etc/project-wildcard/mapi.json')
+const configfile = require('/etc/project-wildcard/mapi.json');
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
 var os = require("os");
 const app = express()
 async function main() {
     var port = configfile.port
+    const key = fs.readFileSync('/etc/project-wildcard/certificates/key.pem');
+    const cert = fs.readFileSync('/etc/project-wildcard/certificates/cert.pem');
+
     app.get("/", async (req, res) => {
         res.send(``);
     })
@@ -15,9 +20,12 @@ async function main() {
     app.post("/login", async (req, res) => {
         res.send(``);
     })
-    app.listen(port, () => {
-        console.log(`Brynet listening at http://localhost:${port}`)
-    })
+
+    // app.listen(port, () => {
+    //     console.log(`Brynet listening at http://localhost:${port}`)
+    // })
+    const server = https.createServer({key: key, cert: cert }, app);
+    server.listen(port, () => { console.log('mapi listening at http://localhost:${port}') });
 }
 
 main();
