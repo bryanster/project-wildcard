bbea58f696d4e04f6ca3ad775ee41f02c25b9c5a
bryanster <bnet1998@gmail.com>
2021-08-21 23:08:56 +0200

    update cert renew script

--- renewcert.sh
+++ renewcert.sh
@@ -6,6 +6,14 @@
 #
 # to create a new certificate and replace existing 
 #
-openssl req  -nodes -new -x509  -keyout ./certutil/server.key -out ./certutil/server.cert
+country=NL
+state=Holland
+locality=Holland
+organization=Project-wildcard
+organizationalunit=IT
+email=""
+
+openssl req  -nodes -new -x509  -keyout ./certutil/server.key -out ./certutil/server.cert\
+     -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
 cp ./certutil/server.key /etc/project-wildcard/certificates/server.key
 cp ./certutil/server.cert /etc/project-wildcard/certificates/server.cert
