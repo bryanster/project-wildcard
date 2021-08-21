4deae6d7eeeb09045e9d8abc79af286b74149594
bryanster <bnet1998@gmail.com>
2021-08-21 22:34:29 +0200

    typo

--- renewcert.sh
+++ renewcert.sh
@@ -7,5 +7,5 @@
 # to create a new certificate and replace existing 
 #
 openssl req  -nodes -new -x509  -keyout ./certutil/server.key -out ./certutil/server.cert
-cp ./certutil/server.key /etc/project-wildcar/certificates/server.key
-cp ./certutil/server.cert /etc/project-wildcar/certificates/server.cert
+cp ./certutil/server.key /etc/project-wildcard/certificates/server.key
+cp ./certutil/server.cert /etc/project-wildcard/certificates/server.cert
