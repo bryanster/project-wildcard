21a43f2f8a20bd5ec65b0ab9337244fad69fde1c
bryanster <bnet1998@gmail.com>
2021-08-21 23:12:58 +0200

    added commonname

--- renewcert.sh
+++ renewcert.sh
@@ -6,6 +6,7 @@
 #
 # to create a new certificate and replace existing 
 #
+commonname=project-wildcard.local
 country=NL
 state=Holland
 locality=Holland
@@ -13,7 +14,7 @@ organization=Project-wildcard
 organizationalunit=IT
 email=certificate@project-wildcard.org
 
-openssl req  -nodes -new -x509  -keyout ./certutil/server.key -out ./certutil/server.cert\
+openssl req  -nodes -new -x509  -keyout ./certutil/server.key -out ./certutil/server.cert \
      -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
 cp ./certutil/server.key /etc/project-wildcard/certificates/server.key
 cp ./certutil/server.cert /etc/project-wildcard/certificates/server.cert
